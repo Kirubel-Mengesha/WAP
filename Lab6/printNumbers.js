134f17a28014f4a18d1adf9ef2447853588b1923
kirasolo <kirubelsolomons280@gmail.com>
2024-05-05 23:54:00 -0500

    Lab6

---
+++ Lab6/printNumbers.js
@@ -0,0 +1,12 @@
+function printNumbers(from, to) {
+    let current = from;
+    const intervalId = setInterval(() => {
+        console.log(current);
+        if (current === to) {
+            clearInterval(intervalId);
+        }
+        current++;
+    }, 1000);
+}
+
+printNumbers(1, 5); // Prints numbers from 1 to 5 with a one-second delay
